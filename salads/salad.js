a25613a159aebb6327ab4ef68c1adc9cb4df9776
Ala-eddin-bosri <bosrialaeddin@gmail.com>
2023-03-09 01:44:45 +0100

    done

--- salads/salad.js
+++ salads/salad.js
@@ -2,3 +2,13 @@
 function back(){
     return window.location.href = "http://127.0.0.1:5500/index.html"
 }
+
+
+function addComment() {
+    var comment = document.getElementById("comment").value;   // Get the user's comment
+    var newComment = document.createElement("li");  // Create a new ligne (li) element to hold the comment
+    var commentText = document.createTextNode(comment);  // Add the comment to li
+    newComment.appendChild(commentText);
+  var position =document.querySelector('li') //position
+    position.insertAdjacentElement("afterend", newComment); // Insert the li element after the empty li
+  }
